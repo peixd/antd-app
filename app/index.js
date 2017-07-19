5ebe552870900564b611d731b7b86b67faa36bbd
simpleasABC <peixd@189.cn>
2017-07-20 03:35:37 +0800

    great step

--- app/index.js
+++ app/index.js
@@ -1,28 +1,134 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import DatePicker from 'antd/lib/date-picker';
-import message from 'antd/lib/message';
-import 'antd/lib/date-picker/style/css';
+import React from 'react';
+import TabBar from 'antd-mobile/lib/tab-bar';
+import Icon from 'antd-mobile/lib/icon';
+import 'antd-mobile/lib/tab-bar/style/css';
+import 'antd-mobile/lib/icon/style/css';
 
-class App extends React.Component {
+/* eslint global-require: 0 */
+
+class TabBarExample extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            date: '',
+            selectedTab: 'redTab',
+            hidden: false,
         };
     }
-    handleChange(date) {
-        message.info('您选择的日期是: ' + date.toString());
-        this.setState({ date });
+
+    renderContent(pageText) {
+        return (
+            <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
+                <div style={{ paddingTop: 60 }}>你已点击“{pageText}” tab， 当前展示“{pageText}”信息</div>
+                <a style={{ display: 'block', marginTop: 40, marginBottom: 600, color: '#108ee9' }}
+                   onClick={(e) => {
+                       e.preventDefault();
+                       this.setState({
+                           hidden: !this.state.hidden,
+                       });
+                   }}
+                >
+                    点击切换 tab-bar 显示/隐藏
+                </a>
+            </div>
+        );
     }
+
     render() {
         return (
-            <div>
-                <DatePicker onChange={value => this.handleChange(value)} />
-                <div style={{ marginTop: 20 }}>当前日期：{this.state.date.toString()}</div>
-            </div>
+            <TabBar
+                unselectedTintColor="#949494"
+                tintColor="#33A3F4"
+                barTintColor="white"
+                hidden={this.state.hidden}
+            >
+                <TabBar.Item
+                    title="生活"
+                    key="生活"
+                    icon={<div style={{
+                        width: '0.44rem',
+                        height: '0.44rem',
+                        background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  0.42rem 0.42rem no-repeat' }}
+                    />
+                    }
+                    selectedIcon={<div style={{
+                        width: '0.44rem',
+                        height: '0.44rem',
+                        background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  0.42rem 0.42rem no-repeat' }}
+                    />
+                    }
+                    selected={this.state.selectedTab === 'blueTab'}
+                    badge={1}
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'blueTab',
+                        });
+                    }}
+                    data-seed="logId"
+                >
+                    {this.renderContent('生活')}
+                </TabBar.Item>
+                <TabBar.Item
+                    icon={<Icon type="koubei-o" size="md" />}
+                    selectedIcon={<Icon type="koubei" size="md" />}
+                    title="口碑"
+                    key="口碑"
+                    badge={'new'}
+                    selected={this.state.selectedTab === 'redTab'}
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'redTab',
+                        });
+                    }}
+                    data-seed="logId1"
+                >
+                    {this.renderContent('口碑')}
+                </TabBar.Item>
+                <TabBar.Item
+                    icon={
+                        <div style={{
+                            width: '0.44rem',
+                            height: '0.44rem',
+                            background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  0.42rem 0.42rem no-repeat' }}
+                        />
+                    }
+                    selectedIcon={
+                        <div style={{
+                            width: '0.44rem',
+                            height: '0.44rem',
+                            background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  0.42rem 0.42rem no-repeat' }}
+                        />
+                    }
+                    title="朋友"
+                    key="朋友"
+                    dot
+                    selected={this.state.selectedTab === 'greenTab'}
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'greenTab',
+                        });
+                    }}
+                >
+                    {this.renderContent('朋友')}
+                </TabBar.Item>
+                <TabBar.Item
+                    icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+                    selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+                    title="我的"
+                    key="我的"
+                    selected={this.state.selectedTab === 'yellowTab'}
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'yellowTab',
+                        });
+                    }}
+                >
+                    {this.renderContent('我的')}
+                </TabBar.Item>
+            </TabBar>
         );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<TabBarExample />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,28 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
-
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+var pxtorem = require('postcss-pxtorem');
+var postcssOpts = {
+    ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+    plugins: () => [
+        autoprefixer({
+            browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4'],
+        }),
+        pxtorem({ rootValue: 100, propWhiteList: [] })
+    ],
+};
 var config = {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index_bundle.js',
         publicPath: '/'
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, 'node_modules'), path.join(__dirname, 'app')],
+        extensions: ['.web.js', '.jsx', '.js', '.json'],
     },
     module: {
         rules: [
@@ -17,7 +32,32 @@ var config = {
                 exclude: [path.resolve(__dirname, "node_modules")],
                 loader: 'babel-loader'
             },
-            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+            { test: /\.(jpg|png)$/, loader: "url-loader?limit=8192" },
+            {
+                test: /\.(svg)$/i, loader: 'svg-sprite-loader', include: [
+                require.resolve('antd-mobile').replace(/warn\.js$/, ''),  // 1. 属于 antd-mobile 内置 svg 文件
+                // path.resolve(__dirname, 'src/my-project-svg-foler'),  // 自己私人的 svg 存放目录
+            ]
+            },
+            // 注意：如下不使用 ExtractTextPlugin 的写法，不能单独 build 出 css 文件
+            // { test: /\.less$/i, loaders: ['style-loader', 'css-loader', 'less-loader'] },
+            // { test: /\.css$/i, loaders: ['style-loader', 'css-loader'] },
+            {
+                test: /\.less$/i, use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader', { loader: 'postcss-loader', options: postcssOpts }, 'less-loader'
+                ]
+            })
+            },
+            {
+                test: /\.css$/i, use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader', { loader: 'postcss-loader', options: postcssOpts }
+                ]
+            })
+            }
         ]
     },
     devServer: {
@@ -26,7 +66,8 @@ var config = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'app/index.html'
-        })
+        }),
+        new ExtractTextPlugin({ filename: '[name].css', allChunks: true })
     ]
 };
 
